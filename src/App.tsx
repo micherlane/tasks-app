38b04f00661f409e22ff4c93234e15f58ee1ecf8
SoLaMente Dev <testercod3@gmail.com>
2023-10-10 08:55:41 -0300

    primeiras rotas configuradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { AboutPage } from './pages/AboutPage'
 import { TasksPage } from './pages/TasksPage'
+import { AddTaskPage } from './pages/AddTaskPage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
+      <div>
       <header>Tasks App Web</header>
-      <main>
-        <TasksPage />
-      </main>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/tasks' element={<TasksPage/>}/>
+        <Route path='/tasks/add' element={<AddTaskPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Routes>
+
       <footer>
         <p>Fim por fim feito por mim!</p>
       </footer>
 
     </div>
+    </BrowserRouter>
+   
   )
 }
 
---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,7 @@
+export function NotFoundPage(){
+    return (
+        <main>
+            <h2>Página não encontrada!</h2>
+        </main>
+    )
+}
