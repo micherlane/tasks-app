68f69e202f0e44741f9f17b519a9105286d12284
SoLaMente Dev <testercod3@gmail.com>
2023-10-10 10:18:43 -0300

    atividade T concluída

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
@@ -15,6 +15,14 @@ function App() {
       <div>
       <header>Tasks App Web</header>
 
+      <nav>
+        <ul>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/tasks'>Tasks</NavLink></li>
+          <li><NavLink to='/login'>Login</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
